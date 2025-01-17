5829f3b2956c8bb4089729740a54395387372348
Alirio Isea <isealirio@gmail.com>
2025-01-17 15:04:35 -0400

    feat: diseño responsivo de las cards del home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -22,7 +22,7 @@ function Home() {
   return (
     <Layout>
       Home
-      <div className="grid gap-6 grid-cols-4 w-full max-w-screen-lg">
+      <div className=" grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {items?.map((item) => (
           <Card key={item.id} data={item} />
         ))}
