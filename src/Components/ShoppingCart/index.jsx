7c9e3fbae4b50a7217f772cff2ceed069233f319
Alirio Isea <isealirio@gmail.com>
2025-02-24 19:14:29 -0400

    style(shopping cart): cambiar colores y grosor de fuente del contador del carrito

--- src/Components/ShoppingCart/index.jsx
+++ src/Components/ShoppingCart/index.jsx
@@ -14,7 +14,7 @@ const ShoppingCart = () => {
   return (
     <div className="relative flex gap-0.5 items-center" onClick={openCart}>
       <ShoppingBagIcon className="text-green-900 size-6 cursor-pointer hover:text-green-950" />
-      <div className="absolute p-1 bottom-3.5 left-3.5 flex justify-center items-center rounded-full bg-green-800 size-4 text-xs text-white">
+      <div className="absolute p-1 bottom-3.5 left-3.5 flex justify-center items-center rounded-full bg-green-50 size-4 text-xs text-green-950 shadow-lg font-semibold">
         {cartProducts.length}
       </div>
     </div>
