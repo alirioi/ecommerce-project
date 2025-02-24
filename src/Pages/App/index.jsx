3a21e91364eeca923e722965b431582522e3baeb
Alirio Isea <isealirio@gmail.com>
2025-02-23 21:47:21 -0400

    refactor(routing): moviendo todas las rutas a un archivo dedicado en la carpeta Routes

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,28 +1,9 @@
-import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ShoppingCartProvider } from '../../Context';
-import Home from '../Home';
-import MyAccount from '../MyAccount';
-import MyOrder from '../MyOrder';
-import MyOrders from '../MyOrders';
-import NotFound from '../NotFound';
-import SignIn from '../SignIn';
+import { AppRoutes } from '../../Routes';
 import Navbar from '../../Components/Navbar';
 import CheckoutSideMenu from '../../Components/CheckoutSideMenu';
 import './App.css';
-
-const AppRoutes = () => {
-  let routes = useRoutes([
-    { path: '/', element: <Home /> },
-    { path: '/:category', element: <Home /> },
-    { path: '/my-account', element: <MyAccount /> },
-    { path: '/my-orders', element: <MyOrders /> },
-    { path: '/my-orders/last', element: <MyOrder /> },
-    { path: '/my-orders/:id', element: <MyOrder /> },
-    { path: '/sign-in', element: <SignIn /> },
-    { path: '/*', element: <NotFound /> },
-  ]);
-  return routes;
-};
 
 const App = () => {
   return (
---
+++ src/Routes/index.jsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { useRoutes } from 'react-router-dom';
+import { ShoppingCartContext } from '../Context';
+
+import Home from '../Pages/Home';
+import MyAccount from '../Pages/MyAccount';
+import MyOrder from '../Pages/MyOrder';
+import MyOrders from '../Pages/MyOrders';
+import NotFound from '../Pages/NotFound';
+import SignIn from '../Pages/SignIn';
+
+export const AppRoutes = () => {
+  const { signOut } = useContext(ShoppingCartContext);
+  return useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/:category', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-orders/last', element: <MyOrder /> },
+    { path: '/my-orders/:id', element: <MyOrder /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ]);
+};
