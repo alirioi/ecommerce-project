dafe44100b3f89532d3e4c67a07c0c45c60a4d55
Alirio Isea <isealirio@gmail.com>
2025-02-24 00:33:14 -0400

    chore(app): inicializar valores de localStorage

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
-import { ShoppingCartProvider } from '../../Context';
+import { ShoppingCartProvider, initializeLocalStorage } from '../../Context';
 import { AppRoutes } from '../../Routes';
 import Navbar from '../../Components/Navbar';
 import CheckoutSideMenu from '../../Components/CheckoutSideMenu';
 import './App.css';
 
 const App = () => {
+  initializeLocalStorage();
   return (
     <ShoppingCartProvider>
       <BrowserRouter>
